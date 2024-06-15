6b269d6ec72452d58c1ef2057c89c34434734abf
mfaoussi <imadfaoussi@gmail.com>
2024-06-15 05:05:44 +0200

    display contact info

--- cpp0/ex01/PhoneBook.cpp
+++ cpp0/ex01/PhoneBook.cpp
@@ -6,7 +6,7 @@
 /*   By: mfaoussi <mfaoussi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/15 03:14:44 by mfaoussi          #+#    #+#             */
-/*   Updated: 2024/06/15 04:59:49 by mfaoussi         ###   ########.fr       */
+/*   Updated: 2024/06/15 05:05:22 by mfaoussi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,5 +49,29 @@ void	PhoneBook::displayContactList() const
 						<< std::setw(10) << truncateField(contacts[i].getLastName()) << "|"
 						<< std::setw(10) << truncateField(contacts[i].getNickName()) << std::endl;
 	}
+}
+
+void	PhoneBook::displayContact(int index) const
+{
+	if (index < 0 || index >= currentSize)
+		std::cout << "Invalid index!" << std::endl;
+	else
+		displayContactInfo(contacts[index]);
+}
+
+void	PhoneBook::displayContactInfo(const Contact& contact) const
+{
+	std::cout << "First name: " << contact.getFirstName() << std::endl;
+	std::cout << "Last name: " << contact.getLastName() << std::endl;
+	std::cout << "Nickname: " << contact.getNickName() << std::endl;
+	std::cout << "Phone number: " << contact.getPhoneNumber() << std::endl;
+	std::cout << "Darkest secret: " << contact.getDarkestSecret() << std::endl;
+}
+
+std::string PhoneBook::truncateField(const std::string& field) const {
+	if (field.length() > 10) {
+		return field.substr(0, 9) + ".";
+	}
+	return field;
 }
 
