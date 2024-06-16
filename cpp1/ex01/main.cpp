2bbb3f3a020edf58a6e7fb883a2da34cf0426713
mfaoussi <imadfaoussi@gmail.com>
2024-06-16 03:59:18 +0200

    creating main for zombieHorde

--- cpp1/ex01/main.cpp
+++ cpp1/ex01/main.cpp
@@ -6,7 +6,27 @@
 /*   By: mfaoussi <mfaoussi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/16 03:40:29 by mfaoussi          #+#    #+#             */
-/*   Updated: 2024/06/16 03:40:30 by mfaoussi         ###   ########.fr       */
+/*   Updated: 2024/06/16 03:57:25 by mfaoussi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+# include "Zombie.hpp"
+Zombie* zombieHorde( int N, std::string name );
+
+int main()
+{
+	int		N;
+	Zombie	*zombies;
+
+	std::string name = "42 Zombies";
+	N = 42;
+	zombies = zombieHorde(N, name);
+	for (int i = 0; i < N; i++)
+	{
+		zombies[i].announce();
+	}
+
+	delete[] zombies;
+	return (0);
+}
 
