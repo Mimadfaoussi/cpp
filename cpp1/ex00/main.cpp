8e6d1376ea088e62d9199cd2035844e704d933c7
mfaoussi <imadfaoussi@gmail.com>
2024-06-16 02:55:02 +0200

    main created

---
+++ cpp1/ex00/main.cpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mfaoussi <mfaoussi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/16 02:38:24 by mfaoussi          #+#    #+#             */
+/*   Updated: 2024/06/16 02:54:43 by mfaoussi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Zombie.hpp"
+Zombie* newZombie( std::string name );
+void	randomChump(std::string name);
+
+int main()
+{
+	Zombie *heapZombie;
+
+	heapZombie = newZombie("imad");
+	heapZombie->announce();
+	randomChump("stack zombie");
+	delete(heapZombie);
+	return (0);
+}
