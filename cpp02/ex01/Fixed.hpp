70169d6907aa7cf9645ceb4a8c035f8a3bd9c02a
mfaoussi <imadfaoussi@gmail.com>
2024-08-07 10:49:45 +0200

    Fixed class implemented and main added

---
+++ cpp02/ex01/Fixed.hpp
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mfaoussi <mfaoussi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/05 09:26:25 by mfaoussi          #+#    #+#             */
+/*   Updated: 2024/08/07 10:34:32 by mfaoussi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# ifndef FIXED_HPP
+# define FIXED_HPP
+
+# include <iostream>
+# include <cmath>
+
+class Fixed {
+
+	public:
+	Fixed();
+	Fixed(const int nb);
+	Fixed(const float nb);
+	~Fixed();
+	Fixed(const Fixed& other);
+	Fixed &operator=(const Fixed& other);
+	friend std::ostream& operator<<(std::ostream& os, const Fixed& fixed_number);
+	int		getRawBits(void) const;
+	void	setRawBits(int const raw);
+	float	toFloat(void) const;
+	int		toInt(void) const;
+	private:
+	int	_n;
+	static const int _bits = 8;
+};
+
+# endif
