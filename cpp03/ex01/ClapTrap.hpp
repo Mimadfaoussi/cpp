466c9cebef42d92f16102a22c43e0d22fb5aa3f2
Mimadfaoussi <imadfaoussi@gmail.com>
2024-08-12 18:54:36 +0200

    creating ScavTrap that inherit from claptrap

---
+++ cpp03/ex01/ClapTrap.hpp
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mfaoussi <mfaoussi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/10 18:11:03 by mfaoussi          #+#    #+#             */
+/*   Updated: 2024/08/10 18:24:47 by mfaoussi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# ifndef CLAPTRAP_HPP
+# define CLAPTRAP_HPP
+
+# include <iostream>
+# include <string>
+
+class ClapTrap {
+	public:
+	ClapTrap();
+	ClapTrap(const std::string& name);
+	ClapTrap(const ClapTrap& other);
+	~ClapTrap();
+
+	ClapTrap& operator=(const ClapTrap& other);
+
+	void			attack(const std::string& target);
+	void			takeDamage(unsigned int amount);
+	void			beRepaired(unsigned int amount);
+
+	protected:
+	std::string		_name;
+	unsigned int 	_hitPoints;
+	unsigned int	_energyPoints;
+	unsigned int	_attackDamage;
+	
+};
+
+# endif
---
+++ cpp03/ex01/ScavTrap.hpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mfaoussi <mfaoussi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/10 18:11:03 by mfaoussi          #+#    #+#             */
+/*   Updated: 2024/08/10 18:24:47 by mfaoussi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# ifndef SCAVTRAP_HPP
+# define SCAVTRAP_HPP
+
+#include "ClapTrap.hpp"
+
+class ScavTrap : public ClapTrap {
+    public:
+
+    ScavTrap();
+    ScavTrap(const std::string& name);
+    ScavTrap(const ScavTrap& other);
+    ScavTrap& operator=(const ScavTrap& other);
+    ~ScavTrap();
+
+    void    attack(const std::string& target);
+    void    guardGate(void);
+
+    private:
+
+};
+
+# endif
