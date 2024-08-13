f454add6cde5ba9d04a34f8f637b397dc60b46e6
mfaoussi <imadfaoussi@gmail.com>
2024-08-13 13:01:10 +0200

    copying files to ex02 from ex01

---
+++ cpp03/ex02/ScavTrap.cpp
@@ -0,0 +1,73 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mfaoussi <mfaoussi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/10 18:11:03 by mfaoussi          #+#    #+#             */
+/*   Updated: 2024/08/10 18:24:47 by mfaoussi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include "ScavTrap.hpp"
+
+ScavTrap::ScavTrap(): ClapTrap("scavtrap")
+{
+    _hitPoints = 100;
+    _energyPoints = 50;
+    _attackDamage = 20;
+    std::cout << "ScavTrap default constructor called" << std::endl;
+}
+
+ScavTrap::ScavTrap(const std::string& name) : ClapTrap(name)
+{
+    _hitPoints = 100;
+    _energyPoints = 50;
+    _attackDamage = 20;
+    std::cout << "ScavTrap "<< _name << " is ready" <<  std::endl;
+}
+
+ScavTrap::ScavTrap(const ScavTrap& other) : ClapTrap(other)
+{
+    std::cout << "ScavTrap " << _name << "copy constructor called" << std::endl;
+    *this = other;
+}
+
+ScavTrap::~ScavTrap()
+{
+    std::cout << "ScavTrap " << _name << " is destroyed" << std::endl;
+}
+
+ScavTrap&   ScavTrap::operator=(ScavTrap const &other)
+{
+    std::cout << "copy assignment operator called " << std::endl;
+    if (this != &other)
+    {
+        _name = other._name;
+        _hitPoints = other._hitPoints;
+        _energyPoints = other._energyPoints;
+        _attackDamage = other._attackDamage;
+    }
+    return (*this);
+}
+
+void    ScavTrap::attack(const std::string& target)
+{
+	if (_energyPoints > 0 && _hitPoints > 0)
+	{
+		_energyPoints = _energyPoints - 1;
+		std::cout << "ScavTrap " << _name << " attacks " << target << " , causing " << _attackDamage << " points of damage!" << std::endl;
+	}
+	else
+	{
+		std::cout << "No energy or hit points left for ClapTrap " << _name <<  std::endl;
+	}
+
+}
+
+void    ScavTrap::guardGate(void)
+{
+    std::cout << "ScavTrap " << _name << " is now in Gate keeper mode " << std::endl;
+}
+
---
+++ cpp03/ex02/main.cpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mfaoussi <mfaoussi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/10 18:25:09 by mfaoussi          #+#    #+#             */
+/*   Updated: 2024/08/10 19:42:46 by mfaoussi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ClapTrap.hpp"
+#include "ScavTrap.hpp"
+
+// int main(void)
+// {
+//     // ClapTrap    clap1("C1");
+//     // ClapTrap    clap2("C2");
+
+//     // clap1.attack("c2");
+//     // clap2.takeDamage(5);
+//     // clap2.beRepaired(10);
+//     // clap2.attack("C1");
+
+    
+// }
