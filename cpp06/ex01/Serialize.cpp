c2452181cdf7cb3948d1be1fa2c3cbae9aceedf2
mfaoussi <imadfaoussi@gmail.com>
2024-11-10 07:23:58 +0100

    creating Serializer class with static methods

--- cpp06/ex01/Serialize.cpp
+++ cpp06/ex01/Serialize.cpp
@@ -6,9 +6,23 @@
 /*   By: mfaoussi <mfaoussi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/10 06:52:00 by mfaoussi          #+#    #+#             */
-/*   Updated: 2024/11/10 06:52:09 by mfaoussi         ###   ########.fr       */
+/*   Updated: 2024/11/10 07:23:29 by mfaoussi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "Serialize.hpp"
+
+Serializer::Serializer(){};
+Serializer::Serializer(const Serializer &other){};
+Serializer::~Serializer(){};
+
+uintptr_t	Serializer::serialize(Data* ptr)
+{
+	return reinterpret_cast <uintptr_t> (ptr);
+}
+
+Data*		Serializer::deserialize(uintptr_t raw)
+{
+	return reinterpret_cast <Data*> (raw);
+}
 
--- cpp06/ex01/Serialize.hpp
+++ cpp06/ex01/Serialize.hpp
@@ -6,13 +6,21 @@
 /*   By: mfaoussi <mfaoussi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/10 06:52:12 by mfaoussi          #+#    #+#             */
-/*   Updated: 2024/11/10 06:55:29 by mfaoussi         ###   ########.fr       */
+/*   Updated: 2024/11/10 07:23:32 by mfaoussi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+# ifndef SERIALIZE_HPP
+# define SERIALIZE_HPP
 
 # include <iostream>
 # include <string>
 
+
+struct Data {
+	int	id;
+	std::string name;
+};
 
 class Serializer {
 	private:
@@ -21,5 +29,9 @@ class Serializer {
 	Serializer& operator=(const Serializer &other);
 	~Serializer();
 
-	
+	public:
+	static uintptr_t serialize(Data* ptr);
+	static Data* deserialize(uintptr_t raw);
 };
+
+# endif
