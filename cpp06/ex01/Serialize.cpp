25c689d88e26c9065d58126a58adc8237256ed75
mfaoussi <imadfaoussi@gmail.com>
2024-11-10 07:32:47 +0100

    main created successfully

--- cpp06/ex01/Serialize.cpp
+++ cpp06/ex01/Serialize.cpp
@@ -6,14 +6,14 @@
 /*   By: mfaoussi <mfaoussi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/10 06:52:00 by mfaoussi          #+#    #+#             */
-/*   Updated: 2024/11/10 07:23:29 by mfaoussi         ###   ########.fr       */
+/*   Updated: 2024/11/10 07:32:20 by mfaoussi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "Serialize.hpp"
 
 Serializer::Serializer(){};
-Serializer::Serializer(const Serializer &other){};
+Serializer::Serializer(const Serializer &other){(void)other;};
 Serializer::~Serializer(){};
 
 uintptr_t	Serializer::serialize(Data* ptr)
--- cpp06/ex01/main.cpp
+++ cpp06/ex01/main.cpp
@@ -6,9 +6,34 @@
 /*   By: mfaoussi <mfaoussi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/10 06:51:47 by mfaoussi          #+#    #+#             */
-/*   Updated: 2024/11/10 06:51:57 by mfaoussi         ###   ########.fr       */
+/*   Updated: 2024/11/10 07:30:32 by mfaoussi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "Serialize.hpp"
 
+int main()
+{
+	Data data;
+	data.id = 42;
+	data.name = "Imad";
+
+	uintptr_t	raw = Serializer::serialize(&data);
+	std::cout << "Serialized data @ = " << raw << std::endl;
+
+	Data*		deserlizedData = Serializer::deserialize(raw);
+
+	if (deserlizedData == &data)
+	{
+		std::cout << "Deserialization successful!" << std::endl;
+		std::cout << "Id : " << deserlizedData->id << std::endl;
+		std::cout << "Name : " << deserlizedData->name << std::endl;
+	}
+	else
+	{
+		std::cout << "Deserialization unsuccessful X__X" << std::endl;
+	}
+
+	return (0);
+
+}
