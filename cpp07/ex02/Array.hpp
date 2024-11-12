35f680fc716afefd33857cc94e2e467ffb9d59af
Mimadfaoussi <imadfaoussi@gmail.com>
2024-11-12 15:04:44 +0100

    exercice 2

---
+++ cpp07/ex02/Array.hpp
@@ -0,0 +1,100 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   iter.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mfaoussi <mfaoussi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/10 17:23:51 by mfaoussi          #+#    #+#             */
+/*   Updated: 2024/11/11 06:41:39 by mfaoussi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# ifndef ARRAY_HPP
+# define ARRAY_HPP
+
+# include <iostream>
+# include <exception>
+
+
+class IndexOutOfRangeException : public std::exception
+{
+	public:
+	char const *what() const throw() 
+	{
+		return "out of range!\n";
+	}
+}
+
+template <typename T>
+class Array {
+	public:
+	Array() : elements(NULL), arraySize(0){}
+	Array(unsigned int n) : arraySize(n)
+	{
+		if (n == 0)
+			elements = NULL;
+		else
+			elements = new T[n]();
+	}
+
+	Array(const Array &other)
+	{
+		arraySize = other.arraySize;
+		if (arraySize == 0)
+			elements = NULL;
+		else
+		{
+			elements = new T[arraySize]();
+			for (unsigned int i = 0; i < arraySize; i++)
+			{
+				elements[i] = other.elements[i];
+			}
+		}
+	}
+	Array& operator=(const Array &other)
+	{
+		if (this != &other)
+		{
+			delete [] elements;
+			arraySize = other.arraySize;
+			if (arraySize > 0)
+			{
+				elements = new T[arraySize]();
+				for (unsigned int i = 0; i < arraySize; i++)
+				{
+					elements[i] = other.elements[i];
+				}
+			}
+		}
+		return (*this);
+	}
+
+	~Array()
+	{
+		std::cout << "elements destroyed" << std::endl;
+		delete [] elements;
+	}
+
+
+	T &operator[](unsigned int index)
+	{
+		if (index >= arraySize)
+			throw IndexOutOfRangeException();
+		return elements[index];
+	}
+
+	T const &operator[](unsigned int index) const 
+	{
+		if (index >= arraySize)
+			throw IndexOutOfRangeException();
+		return elements[index];
+	}
+
+
+	private:
+	T*				elements;
+	unsigned int	arraySize;
+}
+
+# endif
