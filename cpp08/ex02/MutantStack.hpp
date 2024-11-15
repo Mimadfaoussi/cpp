1c7d4c275d0647bd3baeec2ba33a069fd3efa7ff
mfaoussi <imadfaoussi@gmail.com>
2024-11-15 15:45:42 +0100

    MutantStack Iterator and const iterator has been created

---
+++ cpp08/ex02/MutantStack.hpp
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MutantStack.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mfaoussi <mfaoussi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/15 15:02:44 by mfaoussi          #+#    #+#             */
+/*   Updated: 2024/11/15 15:45:07 by mfaoussi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# ifndef MUTANTSTACK_HPP
+# define MUTANTSTACK_HPP
+
+# include <iostream>
+# include <stack>
+
+template <typename T>
+class MutantStack :public std::stack <T>
+{
+	public:
+
+	MutantStack() : std::stack<T>() {}
+	MutantStack(const MutantStack &other) : std::stack<T>(other) {}
+	MutantStack &operator=(const MutantStack &other)
+	{
+		if (this != &other)
+			std::stack<T>::operator=(other);
+		return (*this);
+	}
+	
+	typedef typename std::stack<T>::container_type::iterator iterator;
+	typedef typename std::stack<T>::container_type::const_iterator const_iterator;
+
+	iterator begin()
+	{
+		return this->c.begin();
+	}
+
+	iterator end()
+	{
+		return this->c.end();
+	}
+
+	const_iterator begin() const
+	{
+		return this->c.begin();
+	}
+	const_iterator end() const
+	{
+		return this->c.end();
+	}	
+};
+
+
+# endif
