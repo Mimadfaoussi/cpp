9d89a0fa7e2a4c449b96209bc27e73a436169dba
mfaoussi <imadfaoussi@gmail.com>
2024-11-19 11:06:18 +0100

    checking if the provided args are correct

---
+++ cpp09/ex02/PmergeMe.hpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PmergeMe.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mfaoussi <mfaoussi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/18 10:47:22 by mfaoussi          #+#    #+#             */
+/*   Updated: 2024/11/19 10:42:17 by mfaoussi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# ifndef PMERGEME_HPP
+# define PMERGEME_HPP
+
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <stdexcept>
+#include <cctype>
+#include <climits>
+
+class	PmergeMe {
+	
+};
+
+
+# endif
---
+++ cpp09/ex02/main.cpp
@@ -0,0 +1,68 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mfaoussi <mfaoussi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/18 10:48:21 by mfaoussi          #+#    #+#             */
+/*   Updated: 2024/11/19 11:05:50 by mfaoussi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include "PmergeMe.hpp"
+
+bool	isInteger(const std::string &str)
+{
+	int		start;
+	double	check_limit;
+
+	if (str.empty())
+		throw std::invalid_argument("empty arg.");
+	if (str[0] == '+' || str[1] == '-')
+		start = 1;
+	else
+		start = 0;
+	while (str[start])
+	{
+		if (! std::isdigit(str[start]))
+			throw std::invalid_argument("Invalid integer: " + str);
+		start++;
+	}
+	check_limit = std::stol(str);
+	if (check_limit < INT_MIN || check_limit > INT_MAX)
+		throw std::out_of_range("Integer out of range " + str);
+	return (true);
+}
+
+bool	check_args(char  **argv)
+{
+	int	i;
+
+	try {
+		i = 1;
+		while (argv[i])
+		{
+			isInteger(argv[i]);
+			i++;
+		}
+	} catch (const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+		exit(1);
+	}
+	return (true);
+}
+
+int main(int argc, char **argv)
+{
+	if (argc < 2)
+	{
+		std::cout << "Error: Usage: ./PmergeMe sequence of int's" << std::endl;
+		return (1);
+	}
+	check_args(argv);
+	
+	return (0);
+}
+
